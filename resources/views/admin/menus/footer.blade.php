d85921917a6f39eac4e0fabbdb177162576795f6
marcatao <thiagomarcato@gmail.com>
2021-07-15 08:35:43 -0300

    new version

--- resources/views/admin/menus/footer.blade.php
+++
@@ -1,5 +0,0 @@
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2020-2021 <a href="{{env('APP_URL')}}">{{env('APP_URL')}}</a>.</strong>
-    All rights reserved.
-  </footer>
--- resources/views/mails/name.blade.php
+++
@@ -1,4 +0,0 @@
-<p><b>Nome:</b>{{$request->nome}}</p>
-<p><b>Telefone:</b>{{$request->telefone}}</p>
-<p><b>Email:</b>{{$request->email}}</p>
-<p><b>Mensagem:</b>{{$request->mensagem}}</p>
